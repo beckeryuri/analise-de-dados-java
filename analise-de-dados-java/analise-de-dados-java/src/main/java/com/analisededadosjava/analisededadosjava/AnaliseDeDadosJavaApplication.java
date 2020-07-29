0465d41f777e7a1c00dbb9a2a45ee7edaed6cb39
Yuri Becker <beckercpg@gmail.com>
2020-07-29 05:44:22 -0300

    Iniciando aplicação

---
+++ analise-de-dados-java/analise-de-dados-java/src/main/java/com/analisededadosjava/analisededadosjava/AnaliseDeDadosJavaApplication.java
@@ -0,0 +1,13 @@
+package com.analisededadosjava.analisededadosjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseDeDadosJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaliseDeDadosJavaApplication.class, args);
+	}
+
+}
