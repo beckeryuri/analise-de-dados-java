218dd585b0fec69d9ed152d5056cdda8652c5724
Yuri Becker <beckercpg@gmail.com>
2020-07-29 07:48:17 -0300

    feat(Verificador): adicionar testes unitarios para o novo metodo da classe verificador.

--- analise-de-dados-java/analise-de-dados-java/src/test/java/com/analisededadosjava/analisededadosjava/TesteVerificador.java
+++ analise-de-dados-java/analise-de-dados-java/src/test/java/com/analisededadosjava/analisededadosjava/TesteVerificador.java
@@ -16,4 +16,26 @@ public class TesteVerificador {
         Assertions.assertEquals( "50000", array[3]);
     }
 
+    @Test
+    public void deveVerificarEAdicionarVendedor(){
+        Verificador verificador = new Verificador();
+        String dado = "001ç1234567891234çPedroç50000";
+        verificador.verificaDado(dado);
+        Assertions.assertEquals(1, verificador.getVendedores().getQuantidadeVendedores());
+    }
+    @Test
+    public void deveVerificarEAdicionarCliente(){
+        Verificador verificador = new Verificador();
+        String dado = "002ç2345675434544345çJose da SilvaçRural";
+        verificador.verificaDado(dado);
+        Assertions.assertEquals(1, verificador.getClientes().getQuantidadeClientes());
+    }
+    @Test
+    public void deveVerificarEAdicionarVenda(){
+        Verificador verificador = new Verificador();
+        String dado = "003ç10ç[1-10-100,2-30-2.50,3-40-3.10]çPedro";
+        verificador.verificaDado(dado);
+        Assertions.assertEquals(1, verificador.getVendas().getQuantidadeVendas());
+    }
+
 }
