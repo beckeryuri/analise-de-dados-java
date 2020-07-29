d6c73cfdc3f1e1ad61726f7bc626f9a4e89c5569
Yuri Becker <beckercpg@gmail.com>
2020-07-29 06:30:21 -0300

    feat(Verificador): adicionar classe para organizar e verificar dados recebidos no arquivo.

---
+++ analise-de-dados-java/analise-de-dados-java/src/main/java/com/analisededadosjava/analisededadosjava/Verificador.java
@@ -0,0 +1,10 @@
+package com.analisededadosjava.analisededadosjava;
+
+public class Verificador {
+
+    public String[] divideDado(String dado){
+        String arrayDado[] = dado.split("ç");
+        return arrayDado;
+    }
+
+}
