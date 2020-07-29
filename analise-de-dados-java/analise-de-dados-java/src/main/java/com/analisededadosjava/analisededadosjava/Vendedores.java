776ab045bb08a857c7a906c573b4602938fb6f67
Yuri Becker <beckercpg@gmail.com>
2020-07-29 06:56:16 -0300

    feat(Vendedores): feat(Clientes): criar classe que armazena um arraylist da classe vendedor e criar metodos pra obter informacoes sobre a lista de vendedores.

---
+++ analise-de-dados-java/analise-de-dados-java/src/main/java/com/analisededadosjava/analisededadosjava/Vendedores.java
@@ -0,0 +1,37 @@
+package com.analisededadosjava.analisededadosjava;
+
+import java.util.ArrayList;
+
+public class Vendedores {
+
+    private ArrayList<Vendedor> vendedores = new java.util.ArrayList<>();
+
+    public ArrayList<String> getListaCpf(){
+        ArrayList<String> lista = new ArrayList();
+        String cpf;
+        for(Vendedor vendedor : this.vendedores){
+            cpf = vendedor.getCpf();
+            if(!lista.contains(cpf)){
+                lista.add(cpf);
+            }
+        }
+        return lista;
+    }
+
+    public ArrayList<Vendedor> getVendedores() {
+        return vendedores;
+    }
+
+    public void setVendedores(ArrayList<Vendedor> vendedores) {
+        this.vendedores = vendedores;
+    }
+
+    public int getQuantidadeVendedores(){
+        return vendedores.size();
+    }
+
+    public void adicionarVendedor(Vendedor vendedor){
+        this.vendedores.add(vendedor);
+    }
+    
+}
