1180d836671d612681729aba489a965fa9a4ece2
Yuri Becker <beckercpg@gmail.com>
2020-07-29 08:00:22 -0300

    Feat(AnaliseDeDadosJavaApplication): criar um metodo para monitorar diretorio e realizar a analise de dados toda a vez que um ou mais arquivos forem adicionados.

--- analise-de-dados-java/analise-de-dados-java/src/main/java/com/analisededadosjava/analisededadosjava/AnaliseDeDadosJavaApplication.java
+++ analise-de-dados-java/analise-de-dados-java/src/main/java/com/analisededadosjava/analisededadosjava/AnaliseDeDadosJavaApplication.java
@@ -3,11 +3,30 @@ package com.analisededadosjava.analisededadosjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.*;
+
 @SpringBootApplication
 public class AnaliseDeDadosJavaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(AnaliseDeDadosJavaApplication.class, args);
+
+		WatchService watchService = FileSystems.getDefault().newWatchService();
+
+		Path directory = Paths.get("/home/becker/data/in");
+
+		WatchKey watchKey = directory.register(watchService,StandardWatchEventKinds.ENTRY_CREATE);
+
+		while (true) {
+			for (WatchEvent<?> event : watchKey.pollEvents()) {
+				ManipuladorDeArquivos manipuladorDeArquivos = new ManipuladorDeArquivos();
+				manipuladorDeArquivos.deveEscreverReport(manipuladorDeArquivos);
+				watchKey.pollEvents().clear();
+				manipuladorDeArquivos.deveLimparPasta();
+				break;
+			}
+		}
 	}
 
 }
